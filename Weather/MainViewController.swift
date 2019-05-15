96d75cd37308476efcc99348c4517b04708a6c40
Ali Göktaş <ali.goktas@adesso.com.tr>
2019-05-15 07:55:19 +0300

    Initial commit

---
+++ Weather/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Ali Goktas on 15.05.2019.
+//  Copyright © 2019 adesso. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+

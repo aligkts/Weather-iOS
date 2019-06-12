b96b488c537b02fdc285df11419f515cb13dfca4
Ali Göktaş <ali.goktas@adesso.com.tr>
2019-06-12 08:01:28 +0300

    *Lint warnings fixed *Pod installed

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Weather' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Weather
+  pod 'Alamofire', '~> 5.0.0-beta.5'
+
+  target 'WeatherTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Weather/data/WeatherResponse.swift
+++ Weather/data/WeatherResponse.swift
@@ -1,22 +1,21 @@
-
 import Foundation
 
-struct WeatherResponse : Decodable, Equatable {
+struct WeatherResponse: Decodable, Equatable {
     
-	let coord : Coord?
-	let weather : [Weather]?
-	let base : String?
-	let main : Main?
-	let visibility : Int?
-	let wind : Wind?
-	let clouds : Clouds?
-	let dt : Int?
-	let sys : Sys?
-    let timezone : Int?
-	let id : Int?
-	let name : String?
-	let cod : Int?
-    var uuid : String?
+	let coord: Coord?
+	let weather: [Weather]?
+	let base: String?
+	let main: Main?
+    let visibility: Int?
+	let wind: Wind?
+	let clouds: Clouds?
+	let dt: Int?
+	let sys: Sys?
+    let timezone: Int?
+	let id: Int?
+	let name: String?
+	let cod: Int?
+    var uuid: String?
     var favoriteLocation: FavoriteLocationEntity?
     
     enum CodingKeys: String, CodingKey {
@@ -37,19 +36,19 @@ struct WeatherResponse : Decodable, Equatable {
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
-        coord = try values.decode(Coord.self, forKey: .coord)
-        weather = try values.decode([Weather].self, forKey: .weather)
-        base = try values.decode(String.self, forKey: .base)
-        main = try values.decode(Main.self, forKey: .main)
-        visibility = try values.decode(Int.self, forKey: .visibility)
-        wind = try values.decode(Wind.self, forKey: .wind)
-        clouds = try values.decode(Clouds.self, forKey: .clouds)
-        dt = try values.decode(Int.self, forKey: .dt)
-        sys = try values.decode(Sys.self, forKey: .sys)
-        timezone = try values.decode(Int.self, forKey: .timezone)
-        id = try values.decode(Int.self, forKey: .id)
-        name = try values.decode(String.self, forKey: .name)
-        cod = try values.decode(Int.self, forKey: .cod)
+        coord = try values.decodeIfPresent(Coord.self, forKey: .coord)
+        weather = try values.decodeIfPresent([Weather].self, forKey: .weather)
+        base = try values.decodeIfPresent(String.self, forKey: .base)
+        main = try values.decodeIfPresent(Main.self, forKey: .main)
+        visibility = try values.decodeIfPresent(Int.self, forKey: .visibility)
+        wind = try values.decodeIfPresent(Wind.self, forKey: .wind)
+        clouds = try values.decodeIfPresent(Clouds.self, forKey: .clouds)
+        dt = try values.decodeIfPresent(Int.self, forKey: .dt)
+        sys = try values.decodeIfPresent(Sys.self, forKey: .sys)
+        timezone = try values.decodeIfPresent(Int.self, forKey: .timezone)
+        id = try values.decodeIfPresent(Int.self, forKey: .id)
+        name = try values.decodeIfPresent(String.self, forKey: .name)
+        cod = try values.decodeIfPresent(Int.self, forKey: .cod)
     }
     
     init (resultModel: WeatherResponse) {
